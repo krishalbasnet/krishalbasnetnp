d96e55a96465fcc70131f6e72b952b2d7164b40b
Krishal Basnet <krishalbasnet1@gmail.com>
2023-02-07 08:44:47 +0545

    wwwwwwww

---
+++ New folder (2)/script.js
@@ -0,0 +1,79 @@
+function changefb() {
+  var image = document.getElementById("facebook");
+  image.src = "fb2.png";
+}
+function resetfb() {
+  var image = document.getElementById("facebook");
+  image.src = "fb1.png";
+}
+function not() {
+  var warn = document.getElementById("alert");
+  warn.style.display = "block";
+  setTimeout(function () {
+    warn.style.display = "none";
+  }, 2000);
+}
+
+function changeig() {
+  var image = document.getElementById("instagram");
+  image.src = "ig2.png";
+}
+function resetig() {
+  var image = document.getElementById("instagram");
+  image.src = "ig1.png";
+}
+
+function changeyt() {
+  var image = document.getElementById("youtube");
+  image.src = "yt2.png";
+}
+function resetyt() {
+  var image = document.getElementById("youtube");
+  image.src = "yt1.png";
+}
+
+function changekb() {
+  var image = document.getElementById("krishal");
+  image.src = "kb2.png";
+}
+function resetkb() {
+  var image = document.getElementById("krishal");
+  image.src = "kb1.png";
+}
+
+function changesd() {
+  var image = document.getElementById("soap2day");
+  image.src = "sd2.png";
+}
+function resetsd() {
+  var image = document.getElementById("soap2day");
+  image.src = "sd1.png";
+}
+
+function changech() {
+  var image = document.getElementById("chess");
+  image.src = "ch2.png";
+}
+function resetch() {
+  var image = document.getElementById("chess");
+  image.src = "ch1.png";
+}
+
+function changetw() {
+  var image = document.getElementById("twitter");
+  image.src = "tw2.png";
+}
+function resettw() {
+  var image = document.getElementById("twitter");
+  image.src = "tw1.png";
+}
+
+function changehv() {
+  var image = document.getElementById("hevy");
+  image.src = "hv2.png";
+}
+function resethv() {
+  var image = document.getElementById("hevy");
+  image.src = "hv1.png";
+}
+
