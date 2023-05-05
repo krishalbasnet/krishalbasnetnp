b4508662dfd31ca5315100ea3677f808b0095f56
Krishal Basnet <krishalbasnet1@gmail.com>
2023-05-05 08:09:37 +0545

    dsdsds

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ function bar1(){
     div.appendChild(div2);
     div2.appendChild(a2);
     a2.appendChild(txt2);
+    a2.classList.add("aftereffect");
+    a2.setAttribute("href","index.html");
 
     var div3 = document.createElement("div");
     var a3 = document.createElement("a");
@@ -23,6 +25,7 @@ function bar1(){
     div.appendChild(div3);
     div3.appendChild(a3);
     a3.appendChild(txt3);
+    a3.classList.add("aftereffect");
 
     var div4 = document.createElement("div");
     var a4 = document.createElement("a");
@@ -30,6 +33,7 @@ function bar1(){
     div.appendChild(div4);
     div4.appendChild(a4);
     a4.appendChild(txt4);
+    a4.classList.add("aftereffect");
 
     var div5 = document.createElement("div");
     var a5 = document.createElement("a");
@@ -37,6 +41,7 @@ function bar1(){
     div.appendChild(div5);
     div5.appendChild(a5);
     a5.appendChild(txt5);
+    a5.classList.add("aftereffect");
 
     var div6 = document.createElement("div");
     var a6 = document.createElement("a");
@@ -44,4 +49,5 @@ function bar1(){
     div.appendChild(div6);
     div6.appendChild(a6);
     a6.appendChild(txt6);
+    a6.classList.add("aftereffect");
 }
