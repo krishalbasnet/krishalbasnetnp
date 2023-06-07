c68ba88d69bb790518a0587a6d0174f3ca1ea8ed
Krishal Basnet <krishalbasnet1@gmail.com>
2023-06-08 00:25:34 +0545

    fuckkk

--- c.c
+++ c.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
-#define pi 3.1415
-void main(){
-    float rad, area;
-    printf("Enter the radius: ");
-    scanf("%f",&rad);
-    area = pi * rad * rad;
-    printf("The area is %f",area);
+#include <stdio.h>
+void main()
+{
+    int a, c;
+    printf("Enter the value: ");
+    scanf("%d", &a);
+    c=a%2;
+    (c==0)?(printf("Even")): (printf("Odd."));
 }
