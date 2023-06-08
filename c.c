74062bc6b086633399fe3f547a225a33c3985129
Krishal Basnet <krishalbasnet1@gmail.com>
2023-06-08 07:32:33 +0545

    Update c.c

--- c.c
+++ c.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
-void main()
-{
-    int a, c;
-    printf("Enter the value: ");
-    scanf("%d", &a);
-    c=a%2;
-    (c==0)?(printf("Even")): (printf("Odd."));
-}
+#include<stdio.h> 
+    void main(){
+      int a=15, c=35;
+      float b=20.43;
+      printf("A=%15d|%-15d|%15d|%-15d|%15d|%-15d|%15d|%-15d|%15d|%-15d|\n",a,a,a,a,a,a,a,a,a,a);
+      printf("B=%15.2f|%-15.2f|%15.2f|%-15.2f|%15.2f|%-15.2f|%15.2f|%-15.2f|%15.2f|%-15.2f|\n",b,b,b,b,b,b,b,b,b,b);
+      printf("C=%15d|%-15d|%15d|%-15d|%15d|%-15d|%15d|%-15d|%15d|%-15d|",c,c,c,c,c,c,c,c,c,c);
+    }
