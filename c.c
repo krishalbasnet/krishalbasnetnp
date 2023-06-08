11a449fb8841881c0227a3826920a4f61fe3ac74
Krishal Basnet <krishalbasnet1@gmail.com>
2023-06-09 00:11:25 +0545

    hhh

--- c.c
+++ c.c
@@ -1,10 +1,21 @@
-#include<stdio.h> 
+#include<stdio.h>
+#include<math.h>
 void main(){
-    int i,a,sum=0;
-    printf("Enter 10 numbers: ");
-    for(i=0;i<10;i++){
-        scanf("%d",&a);
-        sum=sum+a;
-    }  
-    printf("The sum is %d",sum);
+    int a,b=0,c,n;
+    printf("Enter a number: ");
+    scanf("%d",&a);
+    c=a;
+    for(n=0;a!=0;n++){
+        a/=10;
+    }
+    a=c;
+    for(a=a;a!=0;a/=10){
+        b = b + pow(a%10,n) ;
+    }
+    if(c==b){
+        printf("It is a armstrong number.");
+    }
+    else{
+        printf("It is not an armstrong number.");
+    }
 }
