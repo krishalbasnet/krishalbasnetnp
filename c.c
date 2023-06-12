db7786564f697f0ec06aa976899807280a67271f
Krishal Basnet <krishalbasnet1@gmail.com>
2023-06-12 09:15:15 +0545

    sss

--- c.c
+++ c.c
@@ -1,14 +1,20 @@
-// find fibonacchi using recursion
+//transpose of matrix
 #include<stdio.h>
 void main(){
-    int n;
-    printf("Enter the number: ");
-    scanf("%d",&n);
-    int fib=0 ;
-    if(x>0){
-        fib = x + fibonacci(x-1);
-    }else{
-        return fib;
+    int matrix[30][30], i, j, m, n;
+    printf("Enter the no of rows and columns: ");
+    scanf("%d%d",&m,&n);
+    printf("Enter the data of the matrix: \n");
+    for(i=0;i<m;i++){   
+        for(j=0;j<n;j++){
+            scanf("%d",&matrix[i][j]);
+        }
     }
-    printf("The fibonacci of %d is %d.",n,fibonacci(n));
+    printf("The transpose is:\n");
+    for(i=0;i<m;i++){   
+        for(j=0;j<n;j++){
+            printf("%d\t",matrix[j][i]);
+        }
+        printf("\n");
+    }
 }
