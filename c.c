317e02e4b70b08ac3758e251ac783a8a0c2aed77
Krishal Basnet <krishalbasnet1@gmail.com>
2023-06-14 16:33:59 +0545

    5 added

--- c.c
+++ c.c
@@ -1,19 +1,15 @@
-#include <stdio.h>
-    #include <math.h>
-    void main(){
-      int n=30, fact = 1, i, j,sign=1;
-      float sum,x;
-      printf("Enter the degree: ");
-      scanf("%f", &x);
-      x=x*3.1415/180;
-      sum = 0.00;
-      for (i = 0; i <= n; i+=2){
-        fact = 1.00;
-        for (j = i; j > 0; j--){
-          fact = fact * j;
-        }
-        sum = sum + sign*(pow(x,i) / (float)fact);
-        sign = sign * -1;
-      }
-      printf("The sum is: %f", sum);
+#include<stdio.h>
+void main(){
+  int i,j,r=5;
+  for(i=r;i>0;i--){
+    printf("\n");
+    for (j = 1; j <= (r - i)*2; j++) {
+    printf(" ");
     }
+    for (j = 1; j <= i; j++) {
+    printf("%d ",j);
+    }
+    for (j = i-1; j >= 1; j--) {
+    printf("%d ",j);
+    }
+}}
