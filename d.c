8f30ee8a2359befec6e52056130b1ae91591c223
Krishal Basnet <krishalbasnet1@gmail.com>
2023-06-21 23:55:46 +0545

    footer added, seperate css files started

--- d.c
+++ d.c
@@ -1,20 +1,36 @@
-// WAP to illustrate the concept of global and static variables.
 #include<stdio.h>
-int a = 20;
-int func1(){
-    static int a1=4;
-    a1++;
-    printf("func1() a1: %d\n",a1);
-    printf("From func1() a: %d\n",a);
-}
-int func2(){
-    a=2;
-    printf("From func2() a: %d\n",a);
-}
 void main(){
-    func1();
-    func1();
-    func2();
-    printf("From main() a: %d\n",a);
-    func2();
+    int r1,c1,r2,c2,i,mat1[50][50],mat2[50][50],mul[50][50],j,k;
+    printf("Enter the no of rows and columns of 1st matrix: ");
+    scanf("%d%d",&r1,&c1);
+    printf("Enter the no of rows and columns of 2nd matrix: ");
+    scanf("%d%d",&r2,&c2);
+    printf("Enter the elements of the 1st matrix:");
+    for(i=0;i<r1;i++){
+        for(j=0;j<c1;j++){
+            scanf("%d",&mat1[i][j]);
+        }
+    }
+    printf("Enter the elements of the 2nd matrix:");
+    for(i=0;i<r2;i++){
+        for(j=0;j<c2;j++){
+            scanf("%d",&mat2[i][j]);
+        }
+    }
+    if(c1==r2){
+        for(i=0;i<r1;i++){    
+            for(j=0;j<c2;j++){    
+                mul[i][j]=0;    
+                for(k=0;k<c2;k++){    
+                    mul[i][j]+=mat1[i][k]*mat2[k][j];    
+                }    
+            }    
+        }    
+    }
+    for(i=0;i<r2;i++){
+        for(j=0;j<c2;j++){
+            printf("%5d",mul[i][j]);
+        }
+        printf("\n");
+    }
 }
