0764a076d2a19b6ee1b197bbd8b5d0b323c4afa7
Krishal Basnet <krishalbasnet1@gmail.com>
2023-06-22 23:13:43 +0545

    commented every shit

--- d.c
+++ d.c
@@ -1,23 +1,4 @@
-#include <stdio.h>
-int fibonacci(int x);
+#include<stdio.h>
 void main(){
-    int n;
-    printf("Enter the number: ");
-    scanf("%d", &n);
-    printf("The %dth fibonacci number is %d.", n, fibonacci(n));
-}
-int fibonacci(int x){
-    int fib = 0;
-    if(x==1){
-        return 0;
-    }
-    else if(x==2){
-        return 1;
-    }
-    else if (x > 0){
-        fib = fibonacci(x-2) + fibonacci(x - 1);
-    }
-    else{
-        return fib;
-    }
+    
 }
