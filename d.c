500f683ff4bd63dfa0b864f374a31fe902ea7351
Krishal Basnet <krishalbasnet1@gmail.com>
2023-06-27 01:39:11 +0545

    sss

--- d.c
+++ d.c
@@ -1,15 +1,17 @@
- #include<stdio.h>
-    void main(){
-      int n,i, num[100], sum=0;
-      float ave;
-      printf("Enter the number of terms: ");
-      scanf("%d",&n);
-      printf("Enter numbers:");
-      for(i=0;i<n;i++){
-        scanf("%d",&num[i]);
-        sum = sum + num[i];
-      }
-      ave = sum / (float)n;
-      printf("The sum is %d and average is %f",sum,ave);
-    }
-        
+// WAP to read two values from the user in main(). Write a function calculate() to calculate sum, product, difference and quotient 
+// of the numbers. Print the sum, product, difference and quotient in the main function using pass by reference concept.
+#include<stdio.h>
+void calculate(int a, int b,int *p, int *q,int *r, float *s){
+  *p = a + b;
+  *q = a - b;
+  *r = a * b;
+  *s = (float)a / b;
+}
+void main(){
+  int a,b,sum,sub,mul;
+  float div;
+  printf("Enter two numbers: ");
+  scanf("%d%d",&a,&b);
+  calculate(a,b,&sum,&sub,&mul,&div);
+  printf("Sum:%d\tDifference:%d\tProduct:%d\tQuotient:%f",sum,sub,mul,div);
+}
