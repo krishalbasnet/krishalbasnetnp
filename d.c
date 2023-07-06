8e682313b52c3eba324a6fffb1ab1b02683d5892
Krishal Basnet <krishalbasnet1@gmail.com>
2023-07-06 23:18:03 +0545

    ssssssssssss

--- d.c
+++ d.c
@@ -1,18 +1,1 @@
-#include <stdio.h>
-void main() {
-    int arr[] = {1, 2, 3, 4, 5};
-    int* ptrArr[5];
-    int(*ptrToArr)[5];           
-    for (int i = 0; i < 5; i++) {
-        ptrArr[i] = &arr[i];
-    }
-    ptrToArr = &arr; 
-    printf("Accessing elements using the array of pointers:\n");
-    for (int i = 0; i < 5; i++) {
-        printf("Value at index %d: %d\n", i, *ptrArr[i]);
-    }
-    printf("\nAccessing elements using the pointer to the array:\n");
-    for (int i = 0; i < 5; i++) {
-        printf("Value at index %d: %d\n", i, (*ptrToArr)[i]);
-    }
-}
+// WAP to read and print the values of structure using pointer to structure.
