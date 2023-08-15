5b45b561fc3e8ee9f706b67bf3e61be05bcd4bc1
Krishal Basnet <krishalbasnet1@gmail.com>
2023-08-15 22:20:53 +0545

    temporary file removed

--- temp/2074 Ashwin/8b.c
+++
@@ -1,3 +0,0 @@
-// Write a program in C to read integers from user until user says "no". After reading the
-// data write all the odd numbers to a file name odd.brt and all the even number to file
-// named even.txt.
--- temp/2074 Ashwin/tempCodeRunnerFile.c
+++
@@ -1,4 +0,0 @@
-for(int j=0;j<i;j++){
-            printf("%c",str1[j]);
-        }
-        printf("\n");
