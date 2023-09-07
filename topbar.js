76b640e3d1432d9751898475e69dd0d5f76ba708
Krishal Basnet <krishalbasnet1@gmail.com>
2023-09-07 21:25:16 +0545

    Create topbar.js

---
+++ topbar.js
@@ -0,0 +1,28 @@
+// JavaScript code to display the provided HTML in the "footer" div
+document.getElementById('topbar').innerHTML = `
+<div class="topbar">
+      <div class="bitem1 baritem">
+        <a class="topbara topbaraimg" href="index.html"
+          ><img class="topbarlogo" src="https://krishalbasnet.com.np/Image/Topbarlogo.png" alt=""
+        /></a>
+      </div>
+      <div class="bitem2 baritem">
+        <a class="topbara" href="https://krishalbasnet.com.np/index.html">Home</a>
+      </div>
+      <div class="bitem3 baritem">
+        <a class="topbara" href="https://krishalbasnet.com.np/solution.html">Solutions</a>
+      </div>
+      <div class="bitem4 baritem">
+        <a class="topbara" href="https://krishalbasnet.com.np/pastpaper.html">Past Paper</a>
+      </div>
+      <div class="bitem5 baritem">
+        <a class="topbara" href="https://krishalbasnet.com.np/notes.html">Notes</a>
+      </div>
+      <div class="bitem6 baritem">
+        <a class="topbara" href="https://krishalbasnet.com.np/questions.html">Questions</a>
+      </div>
+      <div class="bitem7 baritem">
+        <a class="topbara" href="https://krishalbasnet.com.np/contact.html">Contact</a>
+      </div>
+    </div>
+`;
