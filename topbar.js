38f3cc7fc3b6931dd8095fe9963fa94adf237844
Krishal Basnet <krishalbasnet1@gmail.com>
2023-09-26 13:14:04 +0545

    Update topbar.js

--- topbar.js
+++ topbar.js
@@ -2,30 +2,30 @@
 document.getElementById('topbar').innerHTML = `
 <div class="topbar">
       <div class="bitem1 baritem">
-        <a class="topbara topbaraimg" href="https://krishalbasnet.com.np/index.html"
-          ><img class="topbarlogo" src="https://krishalbasnet.com.np/Image/Topbarlogo.png" alt=""
+        <a class="topbara topbaraimg" href="/index.html"
+          ><img class="topbarlogo" src="/Image/Topbarlogo.png" alt=""
         /></a>
       </div>
       <div class="bitem2 baritem">
-        <a class="topbara" href="https://krishalbasnet.com.np/index.html">Home</a>
+        <a class="topbara" href="/index.html">Home</a>
       </div>
       <div class="bitem3 baritem">
-        <a class="topbara" href="https://krishalbasnet.com.np/solution.html">Solutions</a>
+        <a class="topbara" href="/solution.html">Solutions</a>
       </div>
       <div class="bitem4 baritem">
-        <a class="topbara" href="https://krishalbasnet.com.np/pastpaper.html">Past Paper</a>
+        <a class="topbara" href="/pastpaper.html">Past Paper</a>
       </div>
       <div class="bitem5 baritem">
-        <a class="topbara" href="https://krishalbasnet.com.np/classes.html">Classes</a>
+        <a class="topbara" href="/classes.html">Classes</a>
       </div>
       <div class="bitem8 baritem">
-        <a class="topbara" href="https://krishalbasnet.com.np/notes.html">Notes</a>
+        <a class="topbara" href="/notes.html">Notes</a>
       </div>
       <div class="bitem6 baritem">
-        <a class="topbara" href="https://krishalbasnet.com.np/questions.html">Questions</a>
+        <a class="topbara" href="/questions.html">Questions</a>
       </div>
       <div class="bitem7 baritem">
-        <a class="topbara" href="https://krishalbasnet.com.np/support.html">Support</a>
+        <a class="topbara" href="/support.html">Support</a>
       </div>
     </div>
     <center>
