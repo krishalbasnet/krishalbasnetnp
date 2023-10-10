aded44ab276d53d9000fd33c472dc027ad8b2fe8
Krishal Basnet <krishalbasnet1@gmail.com>
2023-10-10 14:36:06 +0545

    Update topbar.js

--- topbar.js
+++ topbar.js
@@ -1,5 +1,6 @@
 // JavaScript code to display the provided HTML in the "footer" div
 document.getElementById('topbar').innerHTML = `
+<div class="toopadministration"><a href="/login.html">Admin</a></div>
 <div class="topbar">
       <div class="bitem1 baritem">
         <a class="topbara topbaraimg" href="/index.html"
@@ -19,7 +20,7 @@ document.getElementById('topbar').innerHTML = `
         <a class="topbara" href="/classes.html">Classes</a>
       </div>
       <div class="bitem8 baritem">
-        <a class="topbara" href="/notes.html">Notes</a>
+        <a class="topbara" href="/library.html">E-Library</a>
       </div>
       <div class="bitem6 baritem">
         <a class="topbara" href="/questions.html">Questions</a>
