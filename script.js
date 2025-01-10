f36e2c7626d3c8a4d347aaa23cddfe4a2e4c7b73
Krishal Basnet <krishalbasnet1@gmail.com>
2025-01-10 15:28:19 +0545

    223

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ topbar.innerHTML = `
             <div class="topitem titem1"><a class="topimage" href="/index.html">Home</a></div>
             <div class="topitem titem2"><a href="/past.html">Past Paper</a></div>
            <div class="topitem titem3"><a href="/library.html">E-Library</a></div> 
-            <div class="topitem titem4"><a href="/cheatsheet.html">Cheatsheet</a></div>
+           <div class="topitem titem4"><a href="/classes.html">Classes</a></div> 
+            <div class="topitem titem5"><a href="/cheatsheet.html">Cheatsheet</a></div>
 
            
         </div>
@@ -18,11 +19,13 @@ footer.innerHTML = `
 <div class="footer">
         <div class="mainfooter">
              <div class="footer1"> 
-                 <!--<div class="footerhead4">FOOTER TOP HEAD</div> 
-                 <div><a href="">TEST</a></div> 
-                 <div><a href="">TEST</a></div> 
-                 <div><a href="">TEST</a></div> 
-                 <div><a href="">TEST</a></div> -->
+<img class="footerimg" src="/Image/The Engineering Junction-logos_white - Copy.png" alt="">
+               
+
+
+
+
+                
              </div> 
              <div class="footer2"> 
                  <!--<div class="footerhead4">FOOTER TOP HEAD</div> 
@@ -32,23 +35,17 @@ footer.innerHTML = `
                  <div><a href="">TEST</a></div> -->
              </div> 
             <div class="footer3">
-                <!--<div class="footerhead4">Information</div>
-                <div><a href="">Old Syllabus</a></div>
+                <!--<div class="footerhead4">Information</div>-->
+                <div><a href="/syll_old.html">Old Syllabus</a></div><!--
                 <div><a href="/syllnew.html">New Syllabus</a></div>
-                <div><a href="">Cut-off</a></div>-->
+                <div><a href="">Cut-off</a></div> -->
             </div>
             <div class="footer4">
-               <!-- <img class="footerimg" src="/Image/The Engineering Junction-logos_white - Copy.png" alt="">
-                <div class="socialfooter">
-                    <a target="_blank" href="https://www.facebook.com/profile.php?id=61550198021764"
-                        class="social2 footerlogoa"><img src="/Image/fb.png" alt=""></a>
-                    <a target="_blank" href="https://www.instagram.com/the_engineering_junction/"
-                        class="social1 footerlogoa"><img src="/Image/ig.png" alt=""></a>
-                    <a target="_blank" href="https://github.com/krishalbasnet" class="social3 footerlogoa"><img
-                            src="/Image/gh.png" alt=""></a>
-                    <a target="_blank" href="https://www.youtube.com/@krishalbasnet" class="social4 footerlogoa"><img
-                            src="/Image/yt.png" alt=""></a>
-                </div>-->
+                <!--<div class="footerhead4">FOOTER TOP HEAD</div> 
+                 <div><a href="">TEST</a></div> 
+                 <div><a href="">TEST</a></div> 
+                 <div><a href="">TEST</a></div> 
+                 <div><a href="">TEST</a></div>  -->
             </div>
         </div>
         <div class="copyrightfooter">
